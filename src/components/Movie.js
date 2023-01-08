1451f1870c0804679bac6ab8d0e98d99a814b3a9
Ba1a123 <118189853+Ba1a123@users.noreply.github.com>
2023-01-08 13:10:20 +0530

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,24 @@
 import React from "react";
-
+import Comment from "./Comment";
+import './movie.css';
+import Ratings from "./Ratings";
 function Movie(props) {
+  console.log("in movie path");
   return (
-    <>
-    console.log("in movie path");
+  <div className="movie">
+    <div className="movie-cont">
+      
+    
+    
     <h1>movie id is {props.id}</h1>
-    </>
+     <div >
+      <Ratings></Ratings>
+    </div>
+    <div>
+    <Comment/>
+    </div>
+    </div>
+  </div>
     );
 }
 
