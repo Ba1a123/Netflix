3bafdfc5c8bccb716e0d0a32171fb23bb0ca1372
Ba1a123 <118189853+Ba1a123@users.noreply.github.com>
2023-01-08 13:09:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import './App.css';
-import React from 'react';
-import requests from './requests';
-import Row from './components/Row';
-import Banner from './components/Banner';
+import "./App.css";
+import React from "react";
+import requests from "./requests";
+import Row from "./components/Row";
+import Banner from "./components/Banner";
 import Nav from "./components/Nav";
-import { Route, Routes  } from 'react-router-dom';
-import Movie from './components/Movie';
+import Movie from "./components/Movie";
+import { Router, Route, Routes } from "react-router-dom";
+import RowRouting from "./components/RowRouting";
+import Comment from "./components/Comment";
 
 function App() {
   return (
     <div className="App">
-        <Routes>
+      <Routes>
+        <Route path="/" element={<RowRouting />} />
         <Route path="/movie" element={<Movie id={23} />} />
-        </Routes>
-      <Nav></Nav>
-      <Banner/>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}></Row>
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
-      <Row title="TOP RATED"fetchUrl={requests.fetchTopRated}/>
-      <Row title="ActionMovies"fetchUrl={requests.fetchActionMovies}/>
-      <Row title="Comedymovies"fetchUrl={requests.fetchComedyMovies}/>
-      <Row title="Horrormovies"fetchUrl={requests.fetchHorrorMovies}/>
-      <Row title="Romancemovies"fetchUrl={requests.fetchRomanceMovies}/>  
-      <Row title="Documentaries"fetchUrl={requests.fetchDocumantaries}/>
+      </Routes>
     </div>
   );
 }
